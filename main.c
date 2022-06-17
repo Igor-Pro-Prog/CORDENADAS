581cd51d3db385c526c9d33b06ba16771fd3bbae
Igor Gabriel <97804247+igorUfca@users.noreply.github.com>
2022-06-17 10:39:20 -0300

    App area Prograd

--- main.c
+++ main.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
-#include "coordenada.h"
+#include <math.h>
+
+/*
+    * Programa que recebe 3 coordenadas e calcula a area usando a formula de heron
+*/
+
+#define QTD_COORDENADAS 3
+
+float distancia(float x1, float y1, float x2, float y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
 
 int main() {
-    ListaCoordenadas *lista = criarListaCoordenadas();
-    // 1 -> continua adicionando | 0 -> Fechar
-    int resposta = 1;
+    float area, coordenadas[2][QTD_COORDENADAS];
 
-    while (resposta == 1) {
-        int x, y;
-
-        printf("Digite a coordenada x: ");
-        scanf("%d", &x);
-
-        printf("Digite a coordenada y: ");
-        scanf("%d", &y);
-
-        Coordenada *novaCoordenada = criarCoordenada(x, y);
-        inserirCoordenada(lista, novaCoordenada);
-
-        printf("Deseja adicionar mais coordenadas? (1 - Sim | 0 - Não)\n");
-        scanf("%d", &resposta);
+    for (int i = 0; i < QTD_COORDENADAS; i++) {
+        printf("COORDENADA %d\n", i + 1);
+        printf("X: ");
+        scanf("%f", &coordenadas[0][i]);
+        printf("Y: ");
+        scanf("%f", &coordenadas[1][i]);
     }
 
-    printf("Lista de coordenadas:\n");
-    imprimirListaCoordenadas(lista);
+    // imprimir todas as coordenadas
+
+    for (int i = 0; i < QTD_COORDENADAS; i++) {
+        printf("COORD %d: %.2f %.2f\n", i + 1, coordenadas[0][i], coordenadas[1][i]);
+    }
+
+    float distancias[QTD_COORDENADAS], perimetro = 0;
+
+    for (int i = 0; i < QTD_COORDENADAS; i++) {
+        int proxI = (i + 1) % QTD_COORDENADAS;
+        printf("ind %d prox %d\n", i, proxI);
+        distancias[i] = distancia(coordenadas[0][i], coordenadas[1][i], coordenadas[0][proxI], coordenadas[1][proxI]);
+        printf("dist %d: %f\n", i + 1, distancias[i]);
+        perimetro += distancias[i];
+    }
+
+    printf("Perimetro: %.2f\n", perimetro);
+
+    float S = perimetro / 2;
+
+    area = sqrt(S * (S - distancias[0]) * (S - distancias[1]) * (S - distancias[2]));
+
+    printf("Area: %.2f\n", area);
 
     return 0;
 }
